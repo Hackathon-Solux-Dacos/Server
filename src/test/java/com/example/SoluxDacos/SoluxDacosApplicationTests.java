08d577b35817cc2ae4bb00be0f5fa3adb73f7826
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-11-06 13:30:35 +0900

    test_ci

---
+++ src/test/java/com/example/SoluxDacos/SoluxDacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoluxDacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoluxDacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
