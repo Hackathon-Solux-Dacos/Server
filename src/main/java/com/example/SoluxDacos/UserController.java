befc9a0a3af267bf2531a968c83a41170a45dfb2
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-11-06 15:04:50 +0900

    fix: ec2 connect

--- src/main/java/com/example/SoluxDacos/UserController.java
+++ src/main/java/com/example/SoluxDacos/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users/cd-test/retest")
+@RequestMapping("/api/users/cd-test/fix_cd")
 public class UserController {
 
     @Autowired
